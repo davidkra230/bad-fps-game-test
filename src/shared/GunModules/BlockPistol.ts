ed387d326b508d21b76f19efa425304fef060d6d
davidkra230 <71524811+davidkra230@users.noreply.github.com>
2022-01-17 18:19:25 -0500

    * optimized the blockpistol module * recompiled (you gotta love coding, made no changes and it works, HOW?)

--- src/shared/GunModules/BlockPistol.ts
+++ src/shared/GunModules/BlockPistol.ts
@@ -1,15 +1,11 @@
 const BlockPistol:any = {};
 
-function multiply(one:number,two:number) {
-return one * two;  
-};
-
 BlockPistol.LeftArmCFrame = new CFrame(1,-0.5,1);
 BlockPistol.RightArmCFrame = new CFrame(1,-0.5,2);
 
-const GunCFrame:number = new CFrame(2,-0.5,-2) as any;
+const GunCFrame:number = new CFrame(2,-0.5,-2);
 const GunAngles:number = CFrame.Angles(math.rad(90),math.rad(-90),math.rad(16)) as any;
 
-BlockPistol.GunCFrame = multiply(GunCFrame,GunAngles);
+BlockPistol.GunCFrame = GunCFrame * GunAngles;
 
 export = BlockPistol;
